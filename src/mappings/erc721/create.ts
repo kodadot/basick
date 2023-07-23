271c38ffb82aea0a4309fa841e8773cfbdd1649d
Viki Val <viktorko99@gmail.com>
2023-07-23 13:42:14 +0200

    :zap: i am erciiiing

--- src/mappings/erc721/create.ts
+++ src/mappings/erc721/create.ts
@@ -1,1 +1,45 @@
-console.log('Hello, world!');
+import { create } from '@kodadot1/metasquid/entity'
+import { CollectionEntity } from '../../model'
+import { Contracts, ContractsMap } from '../../processable'
+import { Context, ItemStateUpdate } from '../utils/types'
+import md5 from 'md5'
+// import { create } from '@kodadot1/metasquid/entity'
+// import md5 from 'md5'
+// import { EMPTY_ADDRESS } from '../utils/constants'
+
+
+// export function initialCollectionState({ event, contract: id }: ItemStateUpdate): Partial<CollectionEntity> {
+//   const final = create(CollectionEntity, id, {})
+
+//   final.blockNumber = BigInt(event.blockNumber)
+//   final.createdAt = event.timestamp
+//   final.currentOwner = EMPTY_ADDRESS
+//   // final.distribution = 0
+//   // final.floor = BigInt(0)
+//   final.hash = md5(id)
+//   // final.highestSale = BigInt(0)
+//   final.id = event.id
+//   final.issuer = EMPTY_ADDRESS
+//   final.max = undefined
+//   // final.metadata = event.metadata
+//   // final.nftCount = 0
+//   // final.ownerCount = 0
+//   // final.supply = 0
+//   final.updatedAt = event.timestamp
+//   // final.volume = BigInt(0)
+//   // final.version = versionOf(context)
+
+
+//   return final
+
+// }
+
+export function handleCollectionCreate(collection: CollectionEntity, context: Context): CollectionEntity {
+  const cache =  ContractsMap[collection.id as Contracts];
+
+  if (cache) {
+    return create(CollectionEntity, collection.id, { ...cache, hash: md5(collection.id) })
+  }
+  
+  return collection
+}
--- src/mappings/erc721/mint.ts
+++ src/mappings/erc721/mint.ts
@@ -1,22 +1,44 @@
+import md5 from 'md5'
 import { contractOf, toBaseEvent, unwrap } from '../utils/extract'
 import { Action, ItemStateUpdate, Log, createTokenId, eventFrom } from '../utils/types'
 import { Transfer } from './utils'
+import { NFTEntity } from '../../model'
+import { create } from '@kodadot1/metasquid/entity'
 
 const OPERATION = Action.MINT
 
 export function handleTokenCreate({ to, tokenId }: Transfer, context: Log): ItemStateUpdate {
   const contract = contractOf(context)
   const base = toBaseEvent(context)
+  const id = createTokenId(contract, tokenId)
   return {
-    id: createTokenId(contract, tokenId),
+    id,
     contract,
     state: {
       currentOwner: to,
       updatedAt: base.timestamp,
-
+      createdAt: base.timestamp,
+      blockNumber: BigInt(base.blockNumber),
+      hash: md5(id),
+      sn: tokenId.toString(),
+      price: 0n,
+      burned: false,
+      lewd: false,
+      version: 721
     },
     interaction: OPERATION,
-    event: eventFrom(OPERATION, base, to)
-  }
+    event: eventFrom(OPERATION, base, to),
+    applyTo(item): NFTEntity {
+      return create(NFTEntity, id, { ...item, ...this.state })
+    },
+    applyFrom(collection) {
+      this.state = {
+        ...this.state,
+        collection,
+        issuer: collection.issuer,
+      }
 
+      return collection
+    }
+  }
 }
