84602efdcbf6d1957cc879f6d48dbc38d9eb2c8f
Viki Val <viktorko99@gmail.com>
2023-07-23 14:26:46 +0200

    :sparkles: how to assign

--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -1,6 +1,7 @@
 
 import { BaseCall, CallWith, Log as Context, UnwrapFunc } from './types'
 
+type Key<T> = keyof T
 
 export function toBaseEvent(context: Context): BaseCall {
   // const caller = addressOf(context.event.extrinsic.signature?.address) 
@@ -20,4 +21,11 @@ export function unwrap<T>(ctx: Context, unwrapFn: UnwrapFunc<T>): CallWith<T> {
   const baseCall = toBaseEvent(ctx);
   const unwrapped = unwrapFn(ctx);
   return { ...baseCall, ...unwrapped };
+}
+
+export function assign<T extends {}>(item: T, state: Partial<T>): void {
+  Object.assign<T, Partial<T>>(item, state)
+  // for (const [key, value] of Object.entries<any>(state)) {
+  //   item[key as Key<T>] = value as T[keyof T]
+  // }
 }
