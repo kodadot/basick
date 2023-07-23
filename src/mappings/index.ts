ee0cdd21df0ae3814e7eb287f53285deb5813424
Viki Val <viktorko99@gmail.com>
2023-07-23 13:42:00 +0200

    :zap: Working POC

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,9 +1,15 @@
 import { serializer } from '@kodadot1/metasquid'
+import { EntityWithId, create } from '@kodadot1/metasquid/entity'
 import * as erc721 from '../abi/ERC721'
 import { handler as handle721Token } from './erc721'
 import { ERC721_TRANSFER } from './erc721/utils'
-import { BlockData, Context, ItemStateUpdate, Log } from './utils/types'
+import { BlockData, Context, EnMap, ItemStateUpdate, Log } from './utils/types'
 import { CONTRACT_ADDRESS } from '../processor'
+import { CollectionEntity as CE, NFTEntity as NE } from '../model'
+import { findByIdListAsMap } from './utils/entity'
+import { uniqueEntitySets } from './utils/unique'
+import { handleCollectionCreate } from './erc721/create'
+import { finalizeCollections } from './utils/lookups'
 
 export async function mainFrame(ctx: Context): Promise<void> {
   const items = []
@@ -21,7 +27,9 @@ export async function mainFrame(ctx: Context): Promise<void> {
     return
   }
 
-  const { contracts, tokens } = uniqueEntities(items)
+  const { contracts, tokens } = uniqueEntitySets(items)
+  const collections = await finalizeCollections(contracts, ctx)
+  const finish = await whatToDoWithTokens({ tokens, collections, items }, ctx)
 
   console.log(JSON.stringify(items, serializer, 2))
 }
@@ -42,12 +50,45 @@ function unwrapLog(log: Log, block: BlockData) {
   }
 }
 
-function uniqueEntities<T extends ItemStateUpdate>(items: T[]): { contracts: Set<string>, tokens: Set<string> } {
-  const contracts = new Set<string>(items.map((i) => i.contract))
-  const tokens = new Set<string>(items.map((i) => i.id))
-
-  return {
-    contracts,
-    tokens,
-  }
+type What = {
+  tokens: Set<string>,
+  collections: EnMap<CE>,
+  items: ItemStateUpdate[],
 }
+
+export async function whatToDoWithTokens({ tokens, collections, items }: What, ctx: Context) {
+  // ctx.store.findBy(CE, {id: In([...collectionMap.keys()])})
+  const knownTokens = await findByIdListAsMap(ctx.store, NE, tokens)
+
+  for (const item of items) {
+    console.log(`APPLY ${item.interaction} on ${item.id}`)
+    const knownToken = knownTokens.get(item.id)
+    if (knownToken) {
+      // update
+      // Object.assign(knownToken, item.state)
+      knownTokens.set(item.id, { ...knownToken, ...item.state })
+    } else {
+      let unknownItem = create(NE, item.id, { })
+      if (item.applyFrom) {
+        const collection = collections.get(item.contract)!
+        item.applyFrom(collection)
+      }
+      if (item.applyTo) {
+        unknownItem = item.applyTo(unknownItem)
+      }
+      
+      knownTokens.set(item.id, unknownItem)
+    }
+
+  }
+
+  const values = [...knownTokens.values()]
+  console.log(JSON.stringify(values, serializer, 2))
+
+  await ctx.store.upsert([...knownTokens.values()])
+  await ctx.store.save([...knownTokens.values()])
+
+  return knownTokens
+}
+
+
