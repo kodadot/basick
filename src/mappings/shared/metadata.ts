e9d4b877f94652d393fb58c0125cfce7a621570b
Viki Val <viktorko99@gmail.com>
2023-07-24 17:46:14 +0200

    :sparkles: handling metadata

---
+++ src/mappings/shared/metadata.ts
@@ -0,0 +1,42 @@
+import { create, get } from '@kodadot1/metasquid/entity'
+import { Optional, TokenMetadata } from '@kodadot1/metasquid/types'
+
+import { logger } from '@kodadot1/metasquid/logger'
+import { MetadataEntity as Metadata } from '../../model/generated'
+import { fetchMetadata } from '../utils/metadata'
+import { Store } from '@subsquid/typeorm-store'
+
+export async function handleMetadata(id: string, store: Store): Promise<Optional<Metadata>> {
+  const meta = await store.get(Metadata, id) // await get<Metadata>(store, Metadata, id)
+  if (meta) {
+    return meta
+  }
+
+  const start = Date.now()
+  const logId = id.split('/').slice(-1).at(0)
+  logger.info(`▶️ [META] ${logId}`)
+  const metadata = await fetchMetadata<TokenMetadata>(id)
+  if (!metadata) {
+    return undefined
+  }
+
+  const partial: Partial<Metadata> = {
+    id,
+    description: metadata.description || '',
+    image: metadata.image || metadata.thumbnailUri || metadata.mediaUri,
+    animationUrl: metadata.animation_url || metadata.mediaUri,
+    attributes: [], // metadata.attributes?.map(attributeFrom) || [],
+    name: metadata.name || '',
+    type: metadata.type || '',
+  }
+
+  const final = create<Metadata>(Metadata, id, partial)
+  const elapsed = (Date.now() - start) / 1000
+  const message = `⏱ [META] ${logId} ${elapsed}s`
+  if (elapsed >= 30) {
+    logger.warn(message)
+  } else {
+    logger.info(message)
+  }
+  return final
+}
---
+++ src/mappings/utils/metadata.ts
@@ -0,0 +1,18 @@
+import { ensure } from '@kodadot1/metasquid'
+import { $obtain } from '@kodadot1/minipfs'
+import logger from './logger'
+// import { attributeFrom } from './types'
+export const BASE_URL = 'https://image.w.kodadot.xyz/'
+
+export const fetchMetadata = async <T>(metadata: string): Promise<T | undefined> => {
+  try {
+    if (!metadata) {
+      return undefined;
+    }
+    return await $obtain<T>(metadata, ['rmrk', 'infura_kodadot1'], true)
+  } catch (e) {
+    logger.error(`[MINIPFS] ${e}}`)
+  }
+
+  return undefined
+}
