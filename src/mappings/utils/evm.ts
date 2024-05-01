46f2d21c7684035306daeda26c7d65df040f317e
Viki Val <viktorko99@gmail.com>
2024-05-01 18:56:01 +0200

    Refactor evm.ts to use generic types for topicOf and decode functions

--- src/mappings/utils/evm.ts
+++ src/mappings/utils/evm.ts
@@ -1,8 +1,17 @@
 import { BlockHeader } from '@subsquid/evm-processor'
 import { Context, Log } from './types'
+import { EventRecord, LogEvent } from '../../abi/abi.support'
 
 
-export function topicOf(ctx: Log): string {
+export function topicOf<T>(ctx: LogEvent<T>): string {
+  return ctx.topic
+}
+
+export function decode<T>(ctx: LogEvent<T>, record: EventRecord): T {
+  return ctx.decode(record)
+}
+
+export function mainTopic(ctx: Log): string {
   return ctx.topics[0]
 }
 
