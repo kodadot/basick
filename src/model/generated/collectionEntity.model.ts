9f0c474d4c31ad662470ad5e89333e01e4502cd0
Viki Val <viktorko99@gmail.com>
2024-05-12 15:42:30 +0200

    :squid: codegen

--- src/model/generated/collectionEntity.model.ts
+++ src/model/generated/collectionEntity.model.ts
@@ -1,5 +1,4 @@
-import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, OneToMany as OneToMany_, Index as Index_, ManyToOne as ManyToOne_} from "typeorm"
-import * as marshal from "./marshal"
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, BigIntColumn as BigIntColumn_, DateTimeColumn as DateTimeColumn_, StringColumn as StringColumn_, IntColumn as IntColumn_, OneToMany as OneToMany_, Index as Index_, ManyToOne as ManyToOne_} from "@subsquid/typeorm-store"
 import {CollectionEvent} from "./collectionEvent.model"
 import {MetadataEntity} from "./metadataEntity.model"
 import {NFTEntity} from "./nftEntity.model"
@@ -11,78 +10,81 @@ export class CollectionEntity {
         Object.assign(this, props)
     }
 
-    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: true})
+    @BigIntColumn_({nullable: true})
     blockNumber!: bigint | undefined | null
 
-    @Column_("timestamp with time zone", {nullable: false})
+    @DateTimeColumn_({nullable: false})
     createdAt!: Date
 
-    @Column_("text", {nullable: false})
+    @StringColumn_({nullable: false})
     currentOwner!: string
 
-    @Column_("int4", {nullable: false})
+    @IntColumn_({nullable: false})
     distribution!: number
 
     @OneToMany_(() => CollectionEvent, e => e.collection)
     events!: CollectionEvent[]
 
-    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    @BigIntColumn_({nullable: false})
     floor!: bigint
 
     @Index_({unique: true})
-    @Column_("text", {nullable: false})
+    @StringColumn_({nullable: false})
     hash!: string
 
-    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    @BigIntColumn_({nullable: false})
     highestSale!: bigint
 
     @PrimaryColumn_()
     id!: string
 
-    @Column_("text", {nullable: true})
+    @StringColumn_({nullable: true})
     image!: string | undefined | null
 
-    @Column_("text", {nullable: false})
+    @StringColumn_({nullable: false})
     issuer!: string
 
-    @Column_("int4", {nullable: true})
+    @IntColumn_({nullable: true})
     max!: number | undefined | null
 
-    @Column_("text", {nullable: true})
+    @StringColumn_({nullable: true})
     media!: string | undefined | null
 
     @Index_()
     @ManyToOne_(() => MetadataEntity, {nullable: true})
     meta!: MetadataEntity | undefined | null
 
-    @Column_("text", {nullable: true})
+    @StringColumn_({nullable: true})
     metadata!: string | undefined | null
 
     @Index_()
-    @Column_("text", {nullable: true})
+    @StringColumn_({nullable: true})
     name!: string | undefined | null
 
-    @Column_("int4", {nullable: false})
+    @IntColumn_({nullable: false})
     nftCount!: number
 
     @OneToMany_(() => NFTEntity, e => e.collection)
     nfts!: NFTEntity[]
 
-    @Column_("int4", {nullable: false})
+    @IntColumn_({nullable: false})
     ownerCount!: number
 
-    @Column_("int4", {nullable: false})
+    @IntColumn_({nullable: false})
     supply!: number
+
+    @StringColumn_({nullable: true})
+    baseUri!: string | undefined | null
 
     @Column_("varchar", {length: 7, nullable: false})
     type!: CollectionType
 
-    @Column_("timestamp with time zone", {nullable: false})
+    @DateTimeColumn_({nullable: false})
     updatedAt!: Date
 
-    @Column_("int4", {nullable: false})
+    @IntColumn_({nullable: false})
     version!: number
 
-    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    @BigIntColumn_({nullable: false})
     volume!: bigint
 }
