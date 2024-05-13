fbf1f3a88166d6abb49e9d46929e14ab6117b1c7
Viki Val <viktorko99@gmail.com>
2024-05-13 15:20:17 +0200

    :loud_sound: debug log

--- src/mappings/utils/lookups.ts
+++ src/mappings/utils/lookups.ts
@@ -1,11 +1,13 @@
 import { CollectionEntity as CE } from '../../model'
 // import { handleCollectionCreate } from '../erc721/create'
 import { findByIdListAsMap } from './entity'
+import logger, { debug } from './logger'
 import { Context, EnMap } from './types'
 
 export async function finalizeCollections(collectionSet: Set<string>, ctx: Context): Promise<EnMap<CE>> {
   // ctx.store.findBy(CE, {id: In([...collectionMap.keys()])})
   const knownCollections = await findByIdListAsMap(ctx.store, CE, collectionSet)
+  debug('finalizeCollections' as any , { knownCollections: knownCollections.size })
   // const newCollections: CE[] = []
 
   // for (const id of collectionSet) {
