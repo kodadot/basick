131164d13a66d2be18461ff89e104a7f83b41fed
Viki Val <viktorko99@gmail.com>
2024-09-06 14:07:07 +0200

    Refactor setMetadata.ts to handle collection metadata updates

--- src/mappings/erc7572/setMetadata.ts
+++ src/mappings/erc7572/setMetadata.ts
@@ -1,9 +1,14 @@
 
 import { merge } from '@kodadot1/metasquid'
+import { getOptional } from '@kodadot1/metasquid/entity'
+import { warn } from 'console'
+import { CollectionEntity as CE } from '../../model'
 import { createEvent } from '../shared/event'
-import { contractOf, toBaseEvent, unwrap } from '../utils/extract'
-import { CollectionStateUpdate, Log, createTokenId, eventFrom } from '../utils/types'
-import { ContractMetadataUpdate } from './utils'
+import { handleMetadata } from '../shared/metadata'
+import { contractOf, toBaseEvent } from '../utils/extract'
+import { pending, success } from '../utils/logger'
+import { CollectionStateUpdate, Context, eventFrom, Log } from '../utils/types'
+import { ContractMetadataUpdate, decode7572ContractUpdate } from './utils'
 
 const OPERATION = 'METADATA' as any
 
@@ -25,4 +30,32 @@ export function handleMetadataSet({ newContractURI: metadata }: ContractMetadata
       return final
     }
   }
+}
+
+export async function handleCollectionMetadataSet(context: Log, process: Context): Promise<void>  {
+  pending(OPERATION, `[COLLECTION/${OPERATION}]: ${context.block.height}`)
+  const contract = contractOf(context)
+  const base = toBaseEvent(context)
+  const event = decode7572ContractUpdate(context)
+  const final = await getOptional(process.store, CE, contract)
+
+  if (!final) {
+    warn(`[COLLECTION/${OPERATION}]: ${contract} not found`)
+    return
+  }
+
+  const oldMetadata = final.metadata
+  final.metadata = event.newContractURI
+  final.updatedAt = base.timestamp
+
+  if (oldMetadata !== final.metadata) {
+    const metadata = await handleMetadata(final.metadata, process.store)
+    final.meta = metadata
+    final.name = metadata?.name || final.name
+    final.image = metadata?.image || final.image
+    final.media = metadata?.animationUrl || final.media
+  }
+
+  await process.store.save(final)
+  success(OPERATION, `[COLLECTION/${OPERATION}] ${final.id}`)
 }
