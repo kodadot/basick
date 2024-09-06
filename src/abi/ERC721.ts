89dd39caf75d9fb9fc60ddf5270912881518850f
Viki Val <viktorko99@gmail.com>
2024-09-06 12:48:10 +0200

    :technologist: from ERC721 to BaseGen

--- src/abi/ERC721.ts
+++ src/abi/ERC721.ts
@@ -5,28 +5,38 @@ import type { EventParams as EParams, FunctionArguments, FunctionReturn } from '
 export const events = {
     Approval: event("0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925", "Approval(address,address,uint256)", {"owner": indexed(p.address), "approved": indexed(p.address), "tokenId": indexed(p.uint256)}),
     ApprovalForAll: event("0x17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31", "ApprovalForAll(address,address,bool)", {"owner": indexed(p.address), "operator": indexed(p.address), "approved": p.bool}),
+    ContractURIUpdated: event("0x905d981207a7d0b6c62cc46ab0be2a076d0298e4a86d0ab79882dbd01ac37378", "ContractURIUpdated(string)", {"newContractURI": p.string}),
+    MaxSupplyUpdated: event("0x7810bd47de260c3e9ee10061cf438099dd12256c79485f12f94dbccc981e806c", "MaxSupplyUpdated(uint256)", {"newMaxSupply": p.uint256}),
+    OwnershipTransferred: event("0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0", "OwnershipTransferred(address,address)", {"previousOwner": indexed(p.address), "newOwner": indexed(p.address)}),
     Transfer: event("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef", "Transfer(address,address,uint256)", {"from": indexed(p.address), "to": indexed(p.address), "tokenId": indexed(p.uint256)}),
 }
 
 export const functions = {
     approve: fun("0x095ea7b3", "approve(address,uint256)", {"to": p.address, "tokenId": p.uint256}, ),
     balanceOf: viewFun("0x70a08231", "balanceOf(address)", {"owner": p.address}, p.uint256),
-    baseURI: viewFun("0x6c0360eb", "baseURI()", {}, p.string),
+    burn: fun("0x42966c68", "burn(uint256)", {"tokenId": p.uint256}, ),
+    contractURI: viewFun("0xe8a3d485", "contractURI()", {}, p.string),
     getApproved: viewFun("0x081812fc", "getApproved(uint256)", {"tokenId": p.uint256}, p.address),
     isApprovedForAll: viewFun("0xe985e9c5", "isApprovedForAll(address,address)", {"owner": p.address, "operator": p.address}, p.bool),
+    maxSupply: viewFun("0xd5abeb01", "maxSupply()", {}, p.uint256),
     name: viewFun("0x06fdde03", "name()", {}, p.string),
     owner: viewFun("0x8da5cb5b", "owner()", {}, p.address),
     ownerOf: viewFun("0x6352211e", "ownerOf(uint256)", {"tokenId": p.uint256}, p.address),
+    renounceOwnership: fun("0x715018a6", "renounceOwnership()", {}, ),
+    royaltyInfo: viewFun("0x2a55205a", "royaltyInfo(uint256,uint256)", {"tokenId": p.uint256, "salePrice": p.uint256}, {"_0": p.address, "_1": p.uint256}),
+    safeBatchMint: fun("0xf9f49952", "safeBatchMint(address,uint256)", {"to": p.address, "quantity": p.uint256}, ),
+    safeMint: fun("0x40d097c3", "safeMint(address)", {"to": p.address}, ),
     'safeTransferFrom(address,address,uint256)': fun("0x42842e0e", "safeTransferFrom(address,address,uint256)", {"from": p.address, "to": p.address, "tokenId": p.uint256}, ),
-    'safeTransferFrom(address,address,uint256,bytes)': fun("0xb88d4fde", "safeTransferFrom(address,address,uint256,bytes)", {"from": p.address, "to": p.address, "tokenId": p.uint256, "_data": p.bytes}, ),
+    'safeTransferFrom(address,address,uint256,bytes)': fun("0xb88d4fde", "safeTransferFrom(address,address,uint256,bytes)", {"from": p.address, "to": p.address, "tokenId": p.uint256, "data": p.bytes}, ),
     setApprovalForAll: fun("0xa22cb465", "setApprovalForAll(address,bool)", {"operator": p.address, "approved": p.bool}, ),
+    setContractURI: fun("0x938e3d7b", "setContractURI(string)", {"newURI": p.string}, ),
+    setMaxSupply: fun("0x6f8b44b0", "setMaxSupply(uint256)", {"newMaxSupply": p.uint256}, ),
     supportsInterface: viewFun("0x01ffc9a7", "supportsInterface(bytes4)", {"interfaceId": p.bytes4}, p.bool),
     symbol: viewFun("0x95d89b41", "symbol()", {}, p.string),
-    tokenByIndex: viewFun("0x4f6ccce7", "tokenByIndex(uint256)", {"index": p.uint256}, p.uint256),
-    tokenOfOwnerByIndex: viewFun("0x2f745c59", "tokenOfOwnerByIndex(address,uint256)", {"owner": p.address, "index": p.uint256}, p.uint256),
     tokenURI: viewFun("0xc87b56dd", "tokenURI(uint256)", {"tokenId": p.uint256}, p.string),
     totalSupply: viewFun("0x18160ddd", "totalSupply()", {}, p.uint256),
     transferFrom: fun("0x23b872dd", "transferFrom(address,address,uint256)", {"from": p.address, "to": p.address, "tokenId": p.uint256}, ),
+    transferOwnership: fun("0xf2fde38b", "transferOwnership(address)", {"newOwner": p.address}, ),
 }
 
 export class Contract extends ContractBase {
@@ -35,8 +45,8 @@ export class Contract extends ContractBase {
         return this.eth_call(functions.balanceOf, {owner})
     }
 
-    baseURI() {
-        return this.eth_call(functions.baseURI, {})
+    contractURI() {
+        return this.eth_call(functions.contractURI, {})
     }
 
     getApproved(tokenId: GetApprovedParams["tokenId"]) {
@@ -45,6 +55,10 @@ export class Contract extends ContractBase {
 
     isApprovedForAll(owner: IsApprovedForAllParams["owner"], operator: IsApprovedForAllParams["operator"]) {
         return this.eth_call(functions.isApprovedForAll, {owner, operator})
+    }
+
+    maxSupply() {
+        return this.eth_call(functions.maxSupply, {})
     }
 
     name() {
@@ -59,20 +73,16 @@ export class Contract extends ContractBase {
         return this.eth_call(functions.ownerOf, {tokenId})
     }
 
+    royaltyInfo(tokenId: RoyaltyInfoParams["tokenId"], salePrice: RoyaltyInfoParams["salePrice"]) {
+        return this.eth_call(functions.royaltyInfo, {tokenId, salePrice})
+    }
+
     supportsInterface(interfaceId: SupportsInterfaceParams["interfaceId"]) {
         return this.eth_call(functions.supportsInterface, {interfaceId})
     }
 
     symbol() {
         return this.eth_call(functions.symbol, {})
-    }
-
-    tokenByIndex(index: TokenByIndexParams["index"]) {
-        return this.eth_call(functions.tokenByIndex, {index})
-    }
-
-    tokenOfOwnerByIndex(owner: TokenOfOwnerByIndexParams["owner"], index: TokenOfOwnerByIndexParams["index"]) {
-        return this.eth_call(functions.tokenOfOwnerByIndex, {owner, index})
     }
 
     tokenURI(tokenId: TokenURIParams["tokenId"]) {
@@ -87,6 +97,9 @@ export class Contract extends ContractBase {
 /// Event types
 export type ApprovalEventArgs = EParams<typeof events.Approval>
 export type ApprovalForAllEventArgs = EParams<typeof events.ApprovalForAll>
+export type ContractURIUpdatedEventArgs = EParams<typeof events.ContractURIUpdated>
+export type MaxSupplyUpdatedEventArgs = EParams<typeof events.MaxSupplyUpdated>
+export type OwnershipTransferredEventArgs = EParams<typeof events.OwnershipTransferred>
 export type TransferEventArgs = EParams<typeof events.Transfer>
 
 /// Function types
@@ -96,14 +109,20 @@ export type ApproveReturn = FunctionReturn<typeof functions.approve>
 export type BalanceOfParams = FunctionArguments<typeof functions.balanceOf>
 export type BalanceOfReturn = FunctionReturn<typeof functions.balanceOf>
 
-export type BaseURIParams = FunctionArguments<typeof functions.baseURI>
-export type BaseURIReturn = FunctionReturn<typeof functions.baseURI>
+export type BurnParams = FunctionArguments<typeof functions.burn>
+export type BurnReturn = FunctionReturn<typeof functions.burn>
+
+export type ContractURIParams = FunctionArguments<typeof functions.contractURI>
+export type ContractURIReturn = FunctionReturn<typeof functions.contractURI>
 
 export type GetApprovedParams = FunctionArguments<typeof functions.getApproved>
 export type GetApprovedReturn = FunctionReturn<typeof functions.getApproved>
 
 export type IsApprovedForAllParams = FunctionArguments<typeof functions.isApprovedForAll>
 export type IsApprovedForAllReturn = FunctionReturn<typeof functions.isApprovedForAll>
+
+export type MaxSupplyParams = FunctionArguments<typeof functions.maxSupply>
+export type MaxSupplyReturn = FunctionReturn<typeof functions.maxSupply>
 
 export type NameParams = FunctionArguments<typeof functions.name>
 export type NameReturn = FunctionReturn<typeof functions.name>
@@ -114,6 +133,18 @@ export type OwnerReturn = FunctionReturn<typeof functions.owner>
 export type OwnerOfParams = FunctionArguments<typeof functions.ownerOf>
 export type OwnerOfReturn = FunctionReturn<typeof functions.ownerOf>
 
+export type RenounceOwnershipParams = FunctionArguments<typeof functions.renounceOwnership>
+export type RenounceOwnershipReturn = FunctionReturn<typeof functions.renounceOwnership>
+
+export type RoyaltyInfoParams = FunctionArguments<typeof functions.royaltyInfo>
+export type RoyaltyInfoReturn = FunctionReturn<typeof functions.royaltyInfo>
+
+export type SafeBatchMintParams = FunctionArguments<typeof functions.safeBatchMint>
+export type SafeBatchMintReturn = FunctionReturn<typeof functions.safeBatchMint>
+
+export type SafeMintParams = FunctionArguments<typeof functions.safeMint>
+export type SafeMintReturn = FunctionReturn<typeof functions.safeMint>
+
 export type SafeTransferFromParams_0 = FunctionArguments<typeof functions['safeTransferFrom(address,address,uint256)']>
 export type SafeTransferFromReturn_0 = FunctionReturn<typeof functions['safeTransferFrom(address,address,uint256)']>
 
@@ -123,17 +154,17 @@ export type SafeTransferFromReturn_1 = FunctionReturn<typeof functions['safeTran
 export type SetApprovalForAllParams = FunctionArguments<typeof functions.setApprovalForAll>
 export type SetApprovalForAllReturn = FunctionReturn<typeof functions.setApprovalForAll>
 
+export type SetContractURIParams = FunctionArguments<typeof functions.setContractURI>
+export type SetContractURIReturn = FunctionReturn<typeof functions.setContractURI>
+
+export type SetMaxSupplyParams = FunctionArguments<typeof functions.setMaxSupply>
+export type SetMaxSupplyReturn = FunctionReturn<typeof functions.setMaxSupply>
+
 export type SupportsInterfaceParams = FunctionArguments<typeof functions.supportsInterface>
 export type SupportsInterfaceReturn = FunctionReturn<typeof functions.supportsInterface>
 
 export type SymbolParams = FunctionArguments<typeof functions.symbol>
 export type SymbolReturn = FunctionReturn<typeof functions.symbol>
-
-export type TokenByIndexParams = FunctionArguments<typeof functions.tokenByIndex>
-export type TokenByIndexReturn = FunctionReturn<typeof functions.tokenByIndex>
-
-export type TokenOfOwnerByIndexParams = FunctionArguments<typeof functions.tokenOfOwnerByIndex>
-export type TokenOfOwnerByIndexReturn = FunctionReturn<typeof functions.tokenOfOwnerByIndex>
 
 export type TokenURIParams = FunctionArguments<typeof functions.tokenURI>
 export type TokenURIReturn = FunctionReturn<typeof functions.tokenURI>
@@ -143,4 +174,7 @@ export type TotalSupplyReturn = FunctionReturn<typeof functions.totalSupply>
 
 export type TransferFromParams = FunctionArguments<typeof functions.transferFrom>
 export type TransferFromReturn = FunctionReturn<typeof functions.transferFrom>
+
+export type TransferOwnershipParams = FunctionArguments<typeof functions.transferOwnership>
+export type TransferOwnershipReturn = FunctionReturn<typeof functions.transferOwnership>
 
